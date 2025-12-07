0f4cbdcbba260ee37afe3300060f758dd3ab66a9
sana <s.mirahsani1998@gmail.com>
2025-12-07 13:12:02 +0100

    create the structure of project

---
+++ Q_learning.py
@@ -0,0 +1,29 @@
+# ==========================================================
+# This file includes the Q-learning alogorithm
+# ==========================================================
+
+# =============================================================================
+# 0. Libraries
+# =============================================================================
+import numpy as np
+import pulp
+import matplotlib.pyplot as plt
+
+# =============================================================================
+# 1. Q_Learning
+# =============================================================================
+def Q_learning():
+    """
+    Q_learning.
+
+    Args:
+        states : set
+            Set of states.
+        actions : set
+            Set of actions.
+        
+    Returns:
+        Q_table : numpy array 2D
+            The Q value for all states and actions pairs.
+    """
+    pass
